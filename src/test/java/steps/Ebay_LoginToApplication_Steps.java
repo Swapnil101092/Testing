482c6ee75e13db9449ae0069a9b5337192406bb0
swapnil.jain <Swapnil@Swapnil-PC>
2021-04-17 23:02:27 +0530

    Added LoginToApplication

---
+++ src/test/java/steps/Ebay_LoginToApplication_Steps.java
@@ -0,0 +1,32 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Ebay_LoginToApplication_Steps {
+	
+	@When("User click on on SignIn Link")
+	public void user_click_on_on_SignIn_Link() {
+	    // Write code here that turns the phrase above into concrete actions
+	 
+	}
+
+	@Then("User navigate to email\\/username field")
+	public void user_navigate_to_email_username_field() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("User enter username and click on continue")
+	public void user_enter_username_and_click_on_continue() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Then("User navigate to ebay home page")
+	public void user_navigate_to_ebay_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	 
+	}
+
+}
