ae696ca7e6748d3acb148cebbf226b97c90081a8
swapnil.jain <Swapnil@Swapnil-PC>
2021-05-16 13:15:12 +0530

    Runner

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,6 +1,12 @@
 package testRunner;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+import com.cucumber.listener.ExtentCucumberFormatter;
+
+import com.cucumber.listener.Reporter;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -8,7 +14,10 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		features = {"features"},glue = {"steps"},plugin = {"pretty","json:Report1"},dryRun = false,strict = true,
+		features = {"features"},
+		glue = {"steps"},
+		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 			
+		dryRun = false,
 		monochrome = true
 	//	tags = {"@P1"}
 		//name = {"Logo"}
@@ -16,5 +25,10 @@ import io.cucumber.junit.CucumberOptions;
 		)
 
 public class TestRunner {
+	@AfterClass
+	public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("config/report.xml"));
+    
+    }
 
 }
